5e2baafc7936fa6996e2cecc1d19713384f8d9c3
Miguel Morales H <49038179+mangelmh@users.noreply.github.com>
2024-05-05 17:24:33 -0600

    Add files via upload

---
+++ melpapp/src/main/java/com/melp/crud/melpapp/MelpappApplication.java
@@ -0,0 +1,13 @@
+package com.melp.crud.melpapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelpappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelpappApplication.class, args);
+	}
+
+}
---
+++ melpapp/src/test/java/com/melp/crud/melpapp/MelpappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melp.crud.melpapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelpappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
